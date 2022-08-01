8e86fd315b7083a42b61bfed6640c32d7a9aefdf
ioannaoiko <ioannaoiko60@gmail.com>
2022-08-01 23:15:03 +0300

    auctions

--- WebApplication/src/main/java/com/example/webapplication/Auction/AuctionController.java
+++ WebApplication/src/main/java/com/example/webapplication/Auction/AuctionController.java
@@ -1,8 +1,13 @@
 package com.example.webapplication.Auction;
 
+import com.example.webapplication.User.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/auctions")
@@ -12,5 +17,23 @@ public class AuctionController {
     @Autowired
     public AuctionController(AuctionService auctionService) {
         this.auctionService = auctionService;
+    }
+
+    @CrossOrigin(origins = "*")
+    @PostMapping()
+    public ResponseEntity<Auction> registerAuctionToBase(@RequestBody Auction auctionForRegistration) {
+        return new ResponseEntity<>(auctionService.registerAuctionToBase(auctionForRegistration), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/getAllActiveAuctions")
+    public List<Auction> getAllActiveAuctions(){
+        return auctionService.getAllActiveAuctions();
+    }
+
+    @CrossOrigin(origins = "*")
+    @PreAuthorize("hasAuthority('AUCTION')")
+    @DeleteMapping("/deleteAuction")
+    public ResponseEntity<?> deleteAuction(@RequestBody Auction auctionForDelete){
+        return new ResponseEntity<>(auctionService.deleteAuction(auctionForDelete),HttpStatus.OK);
     }
 }
--- WebApplication/src/main/java/com/example/webapplication/Auction/AuctionService.java
+++ WebApplication/src/main/java/com/example/webapplication/Auction/AuctionService.java
@@ -1,7 +1,13 @@
 package com.example.webapplication.Auction;
 
+import com.example.webapplication.User.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class AuctionService {
@@ -10,5 +16,19 @@ public class AuctionService {
     @Autowired
     public AuctionService(AuctionRepository auctionRepository) {
         this.auctionRepository = auctionRepository;
+    }
+
+    public Auction registerAuctionToBase(Auction auctionForRegistration) {
+        return auctionRepository.save(auctionForRegistration);
+    }
+
+    public List<Auction> getAllActiveAuctions(){
+        return auctionRepository.findAllWithAuctionEndTimeAfter(LocalDateTime.now());
+
+    }
+
+    public ResponseEntity<?> deleteAuction(Auction auctionToDelete){
+        auctionRepository.deleteById(auctionToDelete.getItemId());
+        return new ResponseEntity<>("Auction has been deleted!", HttpStatus.OK);
     }
 }
